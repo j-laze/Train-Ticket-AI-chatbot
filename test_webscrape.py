75314e525cbf148663ed3ae77f7cd60fa12c6000
silkyspider <james.hamilton.2001@outlook.com>
2024-05-09 08:40:51 +0100

    splitmyfare get_search_url now takes enquirey object, uses all attributes except railcard. added testing for splitmyfare url

---
+++ test_webscrape.py
@@ -0,0 +1,123 @@
+from utils import *
+
+import webscrape.splitmyfare as splitmyfare
+
+
+valid_start_alpha3  = "NRW"
+valid_end_alpha3    = "LST"
+valid_out_time      = "12:00"
+valid_out_date      = "2024-05-11"
+valid_ret_time      = "20:00"
+valid_ret_date      = "2024-05-12"
+
+valid_enquiry_strings_objects = [
+    (
+        "NRW to LST, single, out-depart after mid-day tomorrow, 1 adult",
+        Enquiry(
+            start_alpha3        = valid_start_alpha3,
+            end_alpha3          = valid_end_alpha3,
+            journey_type        = JourneyType.SINGLE,
+            out_time_condition  = TimeCondition.DEPART_AFTER,
+            out_time            = valid_out_time,
+            out_date            = valid_out_date,
+            adults              = 1,
+        )
+    ),
+    (
+        "NRW to LST, single, out-arrive before mid-day tomorrow, 1 adult, 1 child",
+        Enquiry(
+            start_alpha3        = valid_start_alpha3,
+            end_alpha3          = valid_end_alpha3,
+            journey_type        = JourneyType.SINGLE,
+            out_time_condition  = TimeCondition.ARRIVE_BEFORE,
+            out_time            = valid_out_time,
+            out_date            = valid_out_date,
+            adults              = 1,
+            children            = 1,
+        )
+    ),
+    (
+        "NRW to LST, return, out-depart after mid-day tomorrow, ret-depart after 8pm the day after tomorrow, 1 adult",
+        Enquiry(
+            start_alpha3        = valid_start_alpha3,
+            end_alpha3          = valid_end_alpha3,
+            journey_type        = JourneyType.RETURN,
+            out_time_condition  = TimeCondition.DEPART_AFTER,
+            out_time            = valid_out_time,
+            out_date            = valid_out_date,
+            ret_time_condition  = TimeCondition.DEPART_AFTER,
+            ret_time            = valid_ret_time,
+            ret_date            = valid_ret_date,
+            adults              = 1,
+        )
+    ),
+    (
+        "NRW to LST, return, out-arrive before mid-day tomorrow, ret-depart after 8pm the day after tomorrow, 1 adult",
+        Enquiry(
+            start_alpha3        = valid_start_alpha3,
+            end_alpha3          = valid_end_alpha3,
+            journey_type        = JourneyType.RETURN,
+            out_time_condition  = TimeCondition.ARRIVE_BEFORE,
+            out_time            = valid_out_time,
+            out_date            = valid_out_date,
+            ret_time_condition  = TimeCondition.DEPART_AFTER,
+            ret_time            = valid_ret_time,
+            ret_date            = valid_ret_date,
+            adults              = 1,
+        )
+    ),
+    (
+        "NRW to LST, return, out-depart after mid-day tomorrow, ret-arrive before 8pm the day after tomorrow, 1 adult",
+        Enquiry(
+            start_alpha3        = valid_start_alpha3,
+            end_alpha3          = valid_end_alpha3,
+            journey_type        = JourneyType.RETURN,
+            out_time_condition  = TimeCondition.DEPART_AFTER,
+            out_time            = valid_out_time,
+            out_date            = valid_out_date,
+            ret_time_condition  = TimeCondition.ARRIVE_BEFORE,
+            ret_time            = valid_ret_time,
+            ret_date            = valid_ret_date,
+            adults              = 1,
+        )
+    ),
+    (
+        "NRW to LST, return, out-arrive before mid-day tomorrow, ret-arrive before 8pm the day after tomorrow, 1 adult",
+        Enquiry(
+            start_alpha3        = valid_start_alpha3,
+            end_alpha3          = valid_end_alpha3,
+            journey_type        = JourneyType.RETURN,
+            out_time_condition  = TimeCondition.ARRIVE_BEFORE,
+            out_time            = valid_out_time,
+            out_date            = valid_out_date,
+            ret_time_condition  = TimeCondition.ARRIVE_BEFORE,
+            ret_time            = valid_ret_time,
+            ret_date            = valid_ret_date,
+            adults              = 1,
+        )
+    ),
+]
+
+
+
+def test_splitmyfare_url():
+
+    print()
+    for enquiry_string, enquiry_object in valid_enquiry_strings_objects:
+        print(enquiry_string)
+        try:
+            url = splitmyfare.get_search_url(enquiry_object)
+        except Exception as e:
+            print(e)
+            continue
+        print(url)
+        print()
+        
+
+
+    # journeys = splitmyfare.get_journeys(enquiry)
+
+
+
+if __name__ == "__main__":
+    test_splitmyfare_url()
