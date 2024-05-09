f6982f1836d23cfe863178744a8b47bca0ca3169
silkyspider <james.hamilton.2001@outlook.com>
2024-05-09 04:09:28 +0100

    undo pycharms fOrMaTtInG, added git ignore, ignores all __pycache__

--- utils.py
+++ utils.py
@@ -16,39 +16,38 @@ class Enquiry:
 
     def __init__(
             self,
-            start_alpha3: str = None,
-            end_alpha3: str = None,
-            journey_type: JourneyType = None,
-            out_time: str = None,
-            ret_time: str = None,
-            out_time_condition: TimeCondition = None,
-            ret_time_condition: TimeCondition = None,
-            out_date: str = None,
-            ret_date: str = None
-
+            start_alpha3: str = None,                   ## 3 letter code for the start station
+            end_alpha3: str = None,                     ## 3 letter code for the end station
+            journey_type: JourneyType = None,           ## SINGLE or RETURN
+            out_time: str = None,                       ## the time of departure or arrival for the outward journey
+            ret_time: str = None,                       ## the time of departure or arrival for the return journey
+            out_date: str = None,                       ## ON_DEPART or ON_ARRIVE
+            ret_date: str = None,                       ## ON_DEPART or ON_ARRIVE
+            out_time_condition: TimeCondition = None,   ## date and time of departure for the outward journey
+            ret_time_condition: TimeCondition = None,   ## date and time of departure for the return journey
     ):
-        self.start_alpha3 = start_alpha3  ## 3 letter code for the start station
-        self.end_alpha3 = end_alpha3  ## 3 letter code for the end station
-        self.journey_type = journey_type  ## SINGLE or RETURN
-        self.out_time = out_time  ## the time of departure or arrival for the outward journey
-        self.ret_time = ret_time  ## the time of departure or arrival for the return journey
-        self.out_time_condition = out_time_condition  ## ON_DEPART or ON_ARRIVE
-        self.ret_time_condition = ret_time_condition  ## ON_DEPART or ON_ARRIVE
-        self.out_date = out_date  ## date and time of departure for the outward journey
-        self.ret_date = ret_date  ## date and time of departure for the return journey
+        self.start_alpha3 = start_alpha3
+        self.end_alpha3 = end_alpha3
+        self.journey_type = journey_type
+        self.out_time = out_time
+        self.ret_time = ret_time
+        self.out_time_condition = out_time_condition
+        self.ret_time_condition = ret_time_condition
+        self.out_date = out_date
+        self.ret_date = ret_date
 
 
 class Journey:
 
     def __init__(
             self,
-            start_alpha3: str = None,  ## 3 letter code for the start station
-            end_alpha3: str = None,  ## 3 letter code for the end station
-            journey_type: JourneyType = None,  ## SINGLE or RETURN
-            out_depart_time: datetime = None,  ## date and time of departure for the outward journey
-            out_arrive_time: datetime = None,  ## date and time of arrival for the outward journey
-            ret_depart_time: datetime = None,  ## date and time of departure for the return journey
-            ret_arrive_time: datetime = None  ## date and time of arrival for the return journey
+            start_alpha3: str = None,           ## 3 letter code for the start station
+            end_alpha3: str = None,             ## 3 letter code for the end station
+            journey_type: JourneyType = None,   ## SINGLE or RETURN
+            out_depart_time: datetime = None,   ## date and time of departure for the outward journey
+            out_arrive_time: datetime = None,   ## date and time of arrival for the outward journey
+            ret_depart_time: datetime = None,   ## date and time of departure for the return journey
+            ret_arrive_time: datetime = None    ## date and time of arrival for the return journey
     ):
         self.start_alpha3 = start_alpha3
         self.end_alpha3 = end_alpha3
