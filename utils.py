dd5a4c95b0f164cca53979cd59d0893e78185149
silkyspider <james.hamilton.2001@outlook.com>
2024-05-09 04:31:43 +0100

    added Railcard enum, added adults, children and railcard attributes to Enquirey

--- utils.py
+++ utils.py
@@ -6,10 +6,20 @@ class JourneyType(Enum):
     SINGLE = 1
     RETURN = 2
 
-
 class TimeCondition(Enum):
     ON_DEPART = 1
     ON_ARRIVE = 2
+
+class Railcard(Enum):
+    AGE_16_17 = 1
+    AGE_16_25 = 2
+    AGE_26_30 = 3
+    DISABLED = 4
+    FAMILY_FRIEND = 5
+    NETWORK = 6
+    SENIOR = 7
+    TOGETHER = 8
+    VETERAN = 9
 
 
 class Enquiry:
@@ -25,6 +35,9 @@ class Enquiry:
             ret_date: str = None,                       ## ON_DEPART or ON_ARRIVE
             out_time_condition: TimeCondition = None,   ## date and time of departure for the outward journey
             ret_time_condition: TimeCondition = None,   ## date and time of departure for the return journey
+            adults: int = None,                         ## number of adults
+            children: int = None,                       ## number of children
+            railcard: Railcard = None,                  ## the type of railcard
     ):
         self.start_alpha3 = start_alpha3
         self.end_alpha3 = end_alpha3
@@ -35,7 +48,9 @@ class Enquiry:
         self.ret_time_condition = ret_time_condition
         self.out_date = out_date
         self.ret_date = ret_date
-
+        self.adults = adults
+        self.children = children
+        self.railcard = railcard
 
 class Journey:
 
@@ -47,7 +62,7 @@ class Journey:
             out_depart_time: datetime = None,   ## date and time of departure for the outward journey
             out_arrive_time: datetime = None,   ## date and time of arrival for the outward journey
             ret_depart_time: datetime = None,   ## date and time of departure for the return journey
-            ret_arrive_time: datetime = None    ## date and time of arrival for the return journey
+            ret_arrive_time: datetime = None,   ## date and time of arrival for the return journey
     ):
         self.start_alpha3 = start_alpha3
         self.end_alpha3 = end_alpha3
