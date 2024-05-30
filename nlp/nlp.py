34495ffaffd43dee52a61ae67343edd87210ac8b
j-laze <107155977+j-laze@users.noreply.github.com>
2024-05-30 22:56:29 +0100

    added nlp methods for assisting with dialog flow for delay chat, it now has specific methods to pick yp

--- nlp/nlp.py
+++ nlp/nlp.py
@@ -1,7 +1,8 @@
 import pandas as pd
 from itertools import combinations
 
-from utils import JourneyType, TimeCondition
+from utils import JourneyType, TimeCondition, station_df
+
 
 
 def create_patterns(station_data):
@@ -15,7 +16,7 @@ def create_patterns(station_data):
         if pd.isna(station_id):
             continue
         station = station.replace('-', ' ')
-        station_words = station.split()  # Split the station name into words
+        station_words = station.split()
 
         pattern = [{"LOWER": {"FUZZY1": word.lower()}} for word in station_words]
         name_patterns.append({"label": "STATION", "pattern": pattern, "id": station_id})
@@ -27,11 +28,11 @@ def create_patterns(station_data):
             long_name_pattern = [{"LOWER": {"FUZZY1": word.lower()}} for word in long_name_words]
             longname_patterns.append({"label": "STATION", "pattern": long_name_pattern, "id": station_id})
 
-            # If the long name ends with "rail station", create an additional pattern without these words
+            # If the long name ends with "rail station", create a seperate pattern without these words
             if long_name.lower().endswith("rail station"):
                 short_name_words = long_name_words[:-2]
                 for combination in combinations(short_name_words,
-                                                len(short_name_words)):  # Remove the last two words ("rail", "station")
+                                                len(short_name_words)):  # remove the last two words("rail",  "station")
                     short_name_pattern = [{"LOWER": word.lower()} for word in short_name_words]
                     longname_patterns_no_rail_station.append(
                         {"label": "STATION", "pattern": short_name_pattern, "id": station_id})
@@ -42,12 +43,6 @@ def create_patterns(station_data):
     return longname_patterns, names_station_patterns, longname_patterns_no_rail_station, name_patterns
 
 
-def read_csv_to_df():
-    column_names = ["name", "longname", "name_alias", "alpha3", "tiploc"]
-    # Read the CSV file
-    df = pd.read_csv('data/stations.csv', names=column_names, skiprows=1)
-
-    return df
 
 
 def create_entity_ruler(nlp, patterns):
@@ -58,6 +53,19 @@ def create_entity_ruler(nlp, patterns):
     ruler.add_patterns(longname_patterns_no_rail_station)
     ruler.add_patterns(name_patterns)
 
+
+def recognise_station_pred(doc):
+    station_alpha3 = recognise_station(doc)
+    if station_alpha3:
+        station_tiploc = station_df.loc[station_df['alpha3'] == station_alpha3, 'tiploc'].values[0]
+        if station_tiploc in ['CLCHSTR', 'MANNGTR', 'IPSWICH', 'STWMRKT', 'DISS', 'CHLMSFD', 'STFD', 'SHENFLD',
+                              'WITHAME', 'MRKSTEY', 'INGTSTN', 'KELVEDN', 'NEEDHAM', 'HFLPEVL']:
+            return station_tiploc
+    return None
+
+
+
+
 def recognise_chosen_service(doc):
     for token in doc:
         if token.text.lower() == 'delay':
@@ -65,6 +73,8 @@ def recognise_chosen_service(doc):
         elif token.text.lower() == 'ticket':
             return 'prediction'
     return None
+
+
 def recognise_station_directions(doc, user_enquiry):
     for token in doc:
         if token.text in ['to', 'from'] and token.i < len(doc) - 1:
@@ -74,6 +84,7 @@ def recognise_station_directions(doc, user_enquiry):
                     user_enquiry.end_alpha3 = next_token.ent_id_
                 elif token.text == 'from':
                     user_enquiry.start_alpha3 = next_token.ent_id_
+
 
 def recognise_station(doc):
     station_id = None
@@ -111,6 +122,7 @@ def recognise_time_mode(doc, user_enquiry):
     elif last_token in leave_after_phrases:
         user_enquiry.out_time_condition = TimeCondition.DEPART_AFTER
 
+
 def recognise_times(doc,
                     user_enquiry):  # TODO: maybe add a return condition, so i know if the time is for the return journey
     arrive_before_phrases = ['arrive before', 'arrive by', 'arrive at', 'arriving before', 'arriving by', 'arriving at']
@@ -140,9 +152,8 @@ def recognise_dates(doc, user_enquiry):  # maybe add a return condition, so i kn
 ## NOTE: assumes if pm not specified, time is am/24hr
 ## TODO: use some regex to handle more cases and make this more robust
 def fmt_natlang_time(input_str):
-
     input_str = input_str.lower()
-    
+
     if any(phrase in input_str for phrase in ["noon", "midday", "mid day"]):
         return "12:00"
     if any(phrase in input_str for phrase in ["midnight", "mid night"]):
@@ -167,7 +178,7 @@ def fmt_natlang_time(input_str):
         if hour_int < 10:
             return f"0{hour_int}:00"
         return f"{hour_int}:00"
-    
+
     ## if 3 or more digits but no colon return None
     if ":" not in input_str:
         return None
@@ -188,7 +199,6 @@ def fmt_natlang_time(input_str):
     if has_pm:
         hour_int += 12
     return f"{hour_int}:{mins_str}"
-    
 
 
 def print_named_entities_debug(doc):
@@ -203,5 +213,17 @@ def print_time_tokens(doc):
     print("\nTime Entities:")
     for ent in doc.ents:
         if ent.label_ == 'TIME':  # Check if the entity is a time entity
-            print(f"Text: {ent.text}\tStart: {ent.start_char}\tEnd: {ent.end_char}\tLabel: {ent.label_}\tID: {ent.ent_id_}")
+            print(
+                f"Text: {ent.text}\tStart: {ent.start_char}\tEnd: {ent.end_char}\tLabel: {ent.label_}\tID: {ent.ent_id_}")
     print("")
+
+
+def time_to_minutes(time_str):
+    hours, minutes = map(int, time_str.split(':'))
+    return hours * 60 + minutes
+
+
+def minutes_to_time(minutes):
+    hours = minutes // 60
+    minutes = minutes % 60
+    return f"{hours:02d}:{minutes:02d}"
